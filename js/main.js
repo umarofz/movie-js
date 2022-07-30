b39839cef1e46b7995e5ff8f95b162dac528a2b3
umarofz <webnajottalim@gmail.com>
2022-07-31 00:04:00 +0300

    Added filter by year

--- js/main.js
+++ js/main.js
@@ -1,35 +1,63 @@
-let elForm = document.querySelector(".form");
-let elInput = document.querySelector(".rating__input");
-
-let moviesArray = movies.slice(0, 30);
-
-function normolize(array) {
-    let newArray = [];
-    
-    array.forEach(item => {
-        let newObject = {};
-        
-        newObject.title = item.Title.toString();
-        newObject.videoUrl = `https://www.youtube.com/watch?v=${item.ytid}`;
-        newObject.categories = item.Categories.split("|");
-        newObject.movieYear = item.movie_year;
-        newObject.imdbRating = item.imdb_rating;
-        newObject.img = `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`;
-        
-        newArray.push(newObject);
-    });
-    
-    return newArray
-}
-
-
 let elMovieWrapper = document.querySelector(".movie__wrapper");
 let elTemplate = document.querySelector("#movie__card").content;
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".rating__input");
+let elInputYear = document.querySelector(".year__input");
+let elSelectCategories = document.querySelector(".select__categories");
+let elSpan = document.querySelector(".result__span")
 
-let newArray = normolize(moviesArray);
+let moviesArray = movies.slice(0, 20);
+
+let normolizedArray = moviesArray.map(item => {
+    return {
+        title: item.Title.toString(),
+        videoUrl: `https://www.youtube.com/watch?v=${item.ytid}`,
+        categories: item.Categories.split("|"),
+        movieYear: item.movie_year,
+        imdbRating: item.imdb_rating,
+        img: `https://i.ytimg.com/vi/${item.ytid}/mqdefault.jpg`
+    }
+});
+
+function getCategories(array) {
+    let categoriesArray = [];
+    
+    for (const item of array) {
+        for (const itemCategory of item.categories) {
+            if (!categoriesArray.includes(itemCategory)) {
+                categoriesArray.push(itemCategory)
+            }
+        }
+    }
+    
+    return categoriesArray
+}
+
+let movieCategories = getCategories(normolizedArray)
+
+
+// let newArray = normolize(moviesArray);
+
+function renderCategories(array, wrapper) {
+    let tempFragment = document.createDocumentFragment();
+    
+    for (const item of array) {
+        let newOption = document.createElement("option");
+        newOption.textContent = item;
+        newOption.value = item;
+        
+        tempFragment.appendChild(newOption);
+    }
+    
+    wrapper.appendChild(tempFragment)
+}
+renderCategories(movieCategories.sort(), elSelectCategories)
+
+
 
 function moviesRender(array, wrapper) {
     wrapper.innerHTML = null;
+    elSpan.textContent = array.length;
     let tempFragment = document.createDocumentFragment()
     
     for (const item of array) {
@@ -38,26 +66,55 @@ function moviesRender(array, wrapper) {
         templateItem.querySelector(".movie__title").textContent = item.title
         templateItem.querySelector(".movie__year").textContent = item.movieYear
         templateItem.querySelector(".movie__rating").textContent = item.imdbRating
+        templateItem.querySelector(".movie__categories").textContent = item.categories
         templateItem.querySelector(".movie__url").href = item.videoUrl
         
         tempFragment.appendChild(templateItem)
     }
     wrapper.appendChild(tempFragment)
-
+    
     
 }
-moviesRender(newArray, elMovieWrapper)
+moviesRender(normolizedArray, elMovieWrapper);
 
 elForm.addEventListener("submit" , function (evt) {
-    evt.preventDefault();
+    evt.preventDefault()
+    
     let inputValue = Number(document.querySelector(".rating__input").value.trim())
-    let numbersArray = []
-    for (let i = 0; i < newArray.length; i++) {
-        if (inputValue <= newArray[i].imdbRating) {
-            numbersArray.push(newArray[i])
+    if (inputValue) {
+        let numbersArray = []
+        for (let i = 0; i < normolizedArray.length; i++) {
+            if (inputValue <= normolizedArray[i].imdbRating) {
+                numbersArray.push(normolizedArray[i])
+            }
         }
+        moviesRender(numbersArray , elMovieWrapper)
+    } else {
+        let inputValueYear = Number(document.querySelector(".year__input").value.trim())
+        let yearsArray = []
+        for (let i = 0; i < normolizedArray.length; i++) {
+            if (inputValueYear == normolizedArray[i].movieYear) {
+                yearsArray.push(normolizedArray[i])
+            }
+        }
+        moviesRender(yearsArray , elMovieWrapper)
     }
-    moviesRender(numbersArray , elMovieWrapper)
+    
+    
+})
+
+elSelectCategories.addEventListener("input", function (evt) {
+    let selectCategory = elSelectCategories.value;
+    
+    let filteredArray = normolizedArray.filter(function (item) {
+        return item.categories.includes(selectCategory)
+    })
+    
+    if (selectCategory != "all") {
+        moviesRender(filteredArray, elMovieWrapper);
+    } else {
+        moviesRender(normolizedArray, elMovieWrapper);
+    }
 })
 
 
